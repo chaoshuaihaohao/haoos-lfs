f586172eedf5f19808072fb0572cab29d78ff9b2
Hao Chen <chenhaoa@uniontech.com>
2021-04-08 09:17:39 +0800

    modify some scripts for ubuntu build
    add defconfig file for linux kernel.
    
    Signed-off-by: Hao Chen <chenhaoa@uniontech.com>

--- scripts/build_lfs_system1.sh
+++ scripts/build_lfs_system1.sh
@@ -686,6 +686,29 @@ install-info --dir-file=/usr/share/info/dir /usr/share/info/com_err.info
 popd #build
 popd
 
+#BLFS cpio,Linux compile need cpio
+pushd cpio-2.13
+sed -i '/The name/,+2 d' src/global.c
+./configure --prefix=/usr \
+            --bindir=/bin \
+            --enable-mt   \
+            --with-rmt=/usr/libexec/rmt &&
+make &&
+makeinfo --html            -o doc/html      doc/cpio.texi &&
+makeinfo --html --no-split -o doc/cpio.html doc/cpio.texi &&
+makeinfo --plaintext       -o doc/cpio.txt  doc/cpio.texi
+make -C doc pdf &&
+make -C doc ps
+make install &&
+install -v -m755 -d /usr/share/doc/cpio-2.13/html &&
+install -v -m644    doc/html/* \
+                    /usr/share/doc/cpio-2.13/html &&
+install -v -m644    doc/cpio.{html,txt} \
+                    /usr/share/doc/cpio-2.13
+#install -v -m644 doc/cpio.{pdf,ps,dvi} \
+#                 /usr/share/doc/cpio-2.13
+popd
+
 #BLFS iso command
 pushd libuv-v1.41.0
 sh autogen.sh                              &&
