11644785bf2a865829c5118308265b149a53e86a
Hao Chen <chenhaoa@uniontech.com>
2021-04-15 15:27:04 +0800

    modify dep_install.sh for scalability
    Signed-off-by: Hao Chen <chenhaoa@uniontech.com>

--- scripts/dep_install.sh
+++ scripts/dep_install.sh
@@ -4,19 +4,24 @@ if [ `id -u` != 0 ];then
         echo Permission delay, Please run as root!
         exit
 fi
-apt -y install automake
-apt -y install texinfo
-apt -y install g++
-apt -y install bison
-apt -y install flex
-apt -y install binutils
-apt -y install binutils-dev
-apt -y install libiberty-dev
-apt -y install gawk
-##other
-#sudo apt-get install libgcc1 lib32gcc1 libx32gcc1
-#sudo apt-get install build-essential autoconf automake libtool
 
-#ubuntu
-#apt -y install lib32gcc-10-dev libx32gcc-10-dev 
-apt -y install libisl-dev
+DEP_PACKETS="\
+	automake \
+	texinfo \
+	g++ \
+	bison \
+	flex \
+	binutils \
+	binutils-dev \
+	libiberty-dev \
+	gawk \
+	libisl-dev \
+	"
+for i in $DEP_PACKETS
+do
+	dpkg -s $i > /dev/null
+	if [ $? -ne 0 ];then
+		echo dpkg -s $i
+		apt -y $i
+	fi
+done
