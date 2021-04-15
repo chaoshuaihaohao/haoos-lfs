d6d381d68367506c4e63359e27436d3532bc86e3
Hao Chen <chenhaoa@uniontech.com>
2021-04-15 21:08:50 +0800

    fix can't make chroot because of the softlink losed after chroot.
    Signed-off-by: Hao Chen <chenhaoa@uniontech.com>

--- scripts/adduser_lfs.sh
+++ scripts/adduser_lfs.sh
@@ -8,16 +8,10 @@ fi
 echo "export LFS=/mnt/lfs" >  ~/.bash_profile
 source ~/.bash_profile
 
-mkdir -pv $LFS/{etc,var}
-mkdir -pv $LFS/usr/{bin,lib,lib32,libx32,sbin}
-ln -svf $LFS/usr/bin	$LFS/bin
-ln -svf $LFS/usr/lib	$LFS/lib
-ln -svf $LFS/usr/lib32	$LFS/lib32
-ln -svf $LFS/usr/libx32	$LFS/libx32
-ln -svf $LFS/usr/sbin	$LFS/sbin
-
+#4.2. 在 LFS 文件系统中创建有限目录布局
+mkdir -pv $LFS/{bin,etc,lib,sbin,usr,var}
 case $(uname -m) in
-  x86_64) mkdir -pv $LFS/usr/lib64 && ln -svf lib64 $LFS/usr/lib64 ;;
+  x86_64) mkdir -pv $LFS/lib64 ;;
 esac
 
 mkdir -pv $LFS/tools
@@ -31,14 +25,6 @@ useradd -s /bin/bash -g lfs -m -k /dev/null lfs
 echo "Please set the new account lfs's password:"
 passwd lfs
 
-#chown -v -R lfs $LFS/{usr,lib,var,etc,bin,sbin,tools}
-#chgrp -v -R lfs $LFS/{usr,lib,var,etc,bin,sbin,tools}
-#chown -R lfs $LFS/{bin,etc,lfs,lfs-sources.tar.gz,lib,lib64,sbin,sources,tools,usr,var}
-#chgrp -R lfs $LFS/{bin,etc,lfs,lfs-sources.tar.gz,lib,lib64,sbin,sources,tools,usr,var}
-#chown -v lfs $LFS/boot
-#chgrp -v lfs $LFS/boot
-#chown -v -R lfs $LFS
-#chgrp -v -R lfs $LFS
 chown -R lfs $LFS
 chgrp -R lfs $LFS
 case $(uname -m) in
