1c8aa7cbf2b316de9f82d3aa3c40715cbcade5ad
Hao Chen <chenhaoa@uniontech.com>
2021-04-21 14:33:08 +0800

    修正linux相关压缩包的路径
    Signed-off-by: Hao Chen <chenhaoa@uniontech.com>

--- scripts/linux_compile.sh
+++ scripts/linux_compile.sh
@@ -8,18 +8,27 @@ if [ -n $JOBS ];then
         fi
 fi
 export MAKEFLAGS=-j$JOBS
+export LFS_SRC_DIR=/sources/lfs-sources
 
 pushd /lfs
 #10.3. Linux-5.10.17
 #rm -rf linux-5.10.17
-#tar xf /sources/linux-5.10.17.tar.xz
+#tar xf $LFS_SRC_DIR/linux-5.10.17.tar.xz
 pushd linux-5.10.17
 #make mrproper
 #need to copy a .config file from other place.
 ##use the defconfig in linux kernel.
 #make x86_64_defconfig
+patch -Np1 -i $LFS_SRC_DIR/0001-linux-support-aufs-file-system.patch
 make x86_desktop_defconfig
-sed -i 's/CONFIG_SYSTEM_TRUSTED_KEYS\=\"debian\/canonical-certs.pem\"/CONFIG_SYSTEM_TRUSTED_KEYS\=\"\"/' .config
+#sed -i 's/CONFIG_SYSTEM_TRUSTED_KEYS\=\"debian\/canonical-certs.pem\"/CONFIG_SYSTEM_TRUSTED_KEYS\=\"\"/' .config
+#disable BTF
+#sed -i 's/CONFIG_DEBUG_INFO_BTF=y/#\ CONFIG_DEBUG_INFO_BTF\ is\ not\ set/' .config
+#change lz4 to xz
+#sed -i 's/CONFIG_KERNEL_LZ4=y/#\ CONFIG_KERNEL_LZ4\ is\ not\ set/' .config
+#sed -i 's/#\ CONFIG_KERNEL_XZ\ is\ not\ set/CONFIG_KERNEL_XZ=y' .config
+
+
 #make menuconfig
 
 make
@@ -35,7 +44,7 @@ popd #linux-5.10.17
 
 #install linux-firmware
 rm -rf linux-firmware-20210315
-tar xf /sources/linux-firmware-20210315.tar.xz
+tar xf $LFS_SRC_DIR/linux-firmware-20210315.tar.xz
 pushd linux-firmware-20210315
 make install
 popd
