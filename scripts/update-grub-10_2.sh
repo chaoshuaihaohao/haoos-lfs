81c82fdd1c0802714c71ebd332e5357cd86edeec
Hao Chen <chenhaoa@uniontech.com>
2021-04-25 16:20:09 +0800

    grub: add fonts unicode.pf2 in grub.cfg
    fonts file path is sources/other-sources/grub-fonts.tar.xz
    
    Signed-off-by: Hao Chen <chenhaoa@uniontech.com>

--- scripts/update-grub-10_2.sh
+++ scripts/update-grub-10_2.sh
@@ -5,9 +5,9 @@ set -e
 #10.4. 使用 GRUB 设定引导过程
 pushd /tmp
 grub-mkrescue --output=grub-img.iso
-#xorriso -as cdrecord -v dev=/dev/cdrw blank=as_needed grub-img.iso
-#grub-install --grub-setup=/bin/true /dev/vdb -v
-#grub-install --boot-directory=/boot/grub --efi-directory=/boot/efi /dev/vdb -v
 grub-install /dev/vdb -v
+#添加grub字体文件
+tar xf /sources/other-sources/grub-fonts.tar.xz -C /boot/grub/
+mv /boot/grub/grub-fonts /boot/grub/fonts
 grub-mkconfig -o /boot/grub/grub.cfg
 popd
