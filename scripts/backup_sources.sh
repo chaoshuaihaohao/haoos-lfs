fd62852c34a562cab445af8d0b38ad1d124f65a6
Hao Chen <chenhaoa@uniontech.com>
2021-04-25 17:28:27 +0800

    添加软件包sources压缩备份功能
    Signed-off-by: Hao Chen <chenhaoa@uniontech.com>

---
+++ scripts/backup_sources.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e
+if [ `id -u` != 0 ];then
+        echo Permission delay, Please run as root!
+        exit
+fi
+
+
+#备份软件包sources
+if [ -d $LFS ];then
+	export LFS=/mnt/lfs
+else
+	export LFS=/
+fi
+
+pushd $LFS/sources
+tar czf lfs-sources.tar.xz lfs-sources && rm -rf lfs-sources
+tar czf blfs-sources.tar.xz blfs-sources && rm -rf blfs-sources
+tar czf other-sources.tar.xz other-sources && rm -rf other-sources
+popd
+pushd $LFS
+tar czf sources.tar.xz sources && rm -rf sources
+popd
