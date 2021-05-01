76f6906c07b8b5abb321d86b4845429663c1a03c
Hao Chen <chenhaoa@uniontech.com>
2021-05-01 14:52:05 +0800

    规避vim不能多次编译安装问题
    Signed-off-by: Hao Chen <chenhaoa@uniontech.com>

--- scripts/lfs_install-8_2.sh
+++ scripts/lfs_install-8_2.sh
@@ -453,7 +453,9 @@ echo '#define SYS_VIMRC_FILE "/etc/vimrc"' >> src/feature.h
 make
 #chown -Rv tester .
 #su tester -c "LANG=en_US.UTF-8 make -j1 test" &> vim-test.log
+set +e
 make install
+set -e
 ln -sfv vim /usr/bin/vi
 for L in  /usr/share/man/{,*/}man1/vim.1; do
     ln -sfv vim.1 $(dirname $L)/vi.1
