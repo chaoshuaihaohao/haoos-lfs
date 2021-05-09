3814b32eeec3c04d166f2a3fd4cff60db269380c
Hao Chen <chenhaoa@uniontech.com>
2021-05-09 19:58:03 +0800

    修复make-ca编译安装问题
    make-ca依赖wget下载，并且依赖联网，所以需要配置/etc/resolve.conf来获取域名。
    
    Signed-off-by: Hao Chen <chenhaoa@uniontech.com>

--- scripts/chroot-7.sh
+++ scripts/chroot-7.sh
@@ -10,10 +10,8 @@ echo "export LFS=/mnt/lfs" >  ~/.bash_profile
 source ~/.bash_profile
 echo $LFS
 
-#拷贝必要的脚本文件
-mkdir -vp $LFS/haoos/scripts
-cp Makefile $LFS/haoos/
-cp scripts/* $LFS/haoos/scripts/
+#拷贝haoos-lfs文件
+cp -a * $LFS/haoos/
 
 #7.2. 改变$LFS所有者
 chown -R root:root $LFS/{usr,lib,var,etc,bin,sbin,tools}
