eb0f080505d39fdff89d2524ed363aa593a0987c
Hao Chen <chenhaoa@uniontech.com>
2021-05-15 14:03:20 +0800

    添加font配置文件
    Signed-off-by: Hao Chen <chenhaoa@uniontech.com>

--- scripts/blfs_env_config.sh
+++ scripts/blfs_env_config.sh
@@ -358,5 +358,129 @@ export XORG_PREFIX XORG_CONFIG
 EOF
 chmod 644 /etc/profile.d/xorg.sh
 
+#调整Fontconfig
+mkdir -pv ~/.config/fontconfig &&
+cat > ~/.config/fontconfig/fonts.conf << "EOF"
+<?xml version='1.0'?>
+<!DOCTYPE fontconfig SYSTEM 'fonts.dtd'>
+<fontconfig>
+
+  <match target="font" >
+    <!-- autohint was the old automatic hinter when hinting was patent
+    protected, so turn it off to ensure any hinting information in the font
+    itself is used, this is the default -->
+    <edit mode="assign" name="autohint">  <bool>false</bool></edit>
+
+    <!-- hinting is enabled by default -->
+    <edit mode="assign" name="hinting">   <bool>true</bool></edit>
+    
+    <!-- for the lcdfilter see http://www.spasche.net/files/lcdfiltering/ -->
+    <edit mode="assign" name="lcdfilter"> <const>lcddefault</const></edit>
+    
+    <!-- options for hintstyle:
+    hintfull: is supposed to give a crisp font that aligns well to the
+    character-cell grid but at the cost of its proper shape.
+
+    hintmedium: poorly documented, maybe a synonym for hintfull.
+    hintslight is the default: - supposed to be more fuzzy but retains shape.
+    
+    hintnone: seems to turn hinting off.
+    The variations are marginal and results vary with different fonts -->
+    <edit mode="assign" name="hintstyle"> <const>hintslight</const></edit>
+    
+    <!-- antialiasing is on by default and really helps for faint characters
+    and also for 'xft:' fonts used in rxvt-unicode -->
+    <edit mode="assign" name="antialias"> <bool>true</bool></edit>
+    
+    <!-- subpixels are usually rgb, see
+    http://www.lagom.nl/lcd-test/subpixel.php -->
+    <edit mode="assign" name="rgba">      <const>rgb</const></edit>
+    
+    <!-- thanks to the Arch wiki for the lcd and subpixel links -->
+  </match>
+
+</fontconfig>
+EOF
+
+
+#禁用位图字体
+cat > /etc/fonts/conf.d/70-no-bitmaps.conf << "EOF"
+<?xml version='1.0'?>
+<!DOCTYPE fontconfig SYSTEM 'fonts.dtd'>
+<fontconfig>
+<!-- Reject bitmap fonts -->
+ <selectfont>
+  <rejectfont>
+   <pattern>
+     <patelt name="scalable"><bool>false</bool></patelt>
+   </pattern>
+  </rejectfont>
+ </selectfont>
+</fontconfig>
+EOF
+
+#添加额外的字体目录
+cat > /etc/fonts/conf.d/09-texlive.conf << "EOF"
+<?xml version='1.0'?>
+<!DOCTYPE fontconfig SYSTEM 'fonts.dtd'>
+<fontconfig>
+  <dir>/opt/texlive/2020/texmf-dist/fonts/opentype/arkandis/berenisadf</dir>
+  <dir>/opt/texlive/2020/texmf-dist/fonts/truetype/paratype</dir>
+</fontconfig>
+EOF
+
+#首选特定字体
+mkdir -pv ~/.config/fontconfig/conf.d &&
+cat >  ~/.config/fontconfig/conf.d/35-prefer-nimbus-for-timesnew.conf << "EOF"
+<?xml version='1.0'?>
+<!DOCTYPE fontconfig SYSTEM 'fonts.dtd'>
+<fontconfig>
+<!-- prefer Nimbus Roman No9 L for Times New Roman as well as for Times,
+ without this Tinos and Liberation Serif take precedence for Times New Roman
+ before fontconfig falls back to whatever matches Times -->
+    <alias binding="same">
+        <family>Times New Roman</family>
+        <accept>
+            <family>Nimbus Roman No9 L</family>
+        </accept>
+    </alias>
+</fontconfig>
+EOF
+
+
+#优先选择CJK字体
+cat > /etc/fonts/local.conf << "EOF"
+<?xml version='1.0'?>
+<!DOCTYPE fontconfig SYSTEM 'fonts.dtd'>
+<fontconfig>
+    <alias>
+        <family>serif</family>
+        <prefer>
+            <family>AR PL UMing</family>
+            <family>IPAexMincho</family>
+            <!-- WenQuanYi is preferred as Serif in 65-nonlatin.conf,
+            override that so a real Korean font can be used for Serif -->
+            <family>UnBatang</family>
+        </prefer>
+    </alias>
+    <alias>
+         <family>sans-serif</family>
+         <prefer>
+             <family>WenQuanYi Zen Hei</family>
+             <family>VL Gothic</family>
+             <family>IPAexGothic</family>
+         </prefer>
+    </alias>
+    <alias>
+         <family>monospace</family>
+         <prefer>
+             <family>VL Gothic</family>
+             <family>IPAexGothic</family>
+             <family>WenQuanYi Zen Hei</family>
+         </prefer>
+    </alias>
+</fontconfig>
+EOF
+
 
 popd #LFS
