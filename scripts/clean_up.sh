befed7194b8262fd28136bcb62afb313cd983bef
Hao Chen <chenhaoa@uniontech.com>
2021-05-24 15:36:27 +0800

    optimize Makefile scripts order
    Signed-off-by: Hao Chen <chenhaoa@uniontech.com>

--- scripts/clean_up.sh
+++ scripts/clean_up.sh
@@ -2,8 +2,12 @@
 set -e
 #Chapter 8
 #8.77. Cleaning Up
+rm -rf /tmp/*
 
 find /usr/lib /usr/libexec -name \*.la -delete
 find /usr -depth -name $(uname -m)-lfs-linux-gnu\* | xargs rm -rf
 rm -rf /tools
+
+set +e
 userdel -rf tester
+set -e
