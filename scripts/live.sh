52ce0c3ca8e3a57c31715850245649f11100489e
Hao Chen <chenhaoa@uniontech.com>
2021-06-03 20:31:33 +0800

    scripts: 合并livecd && liveusb为live
    通过make iso命令生成live iso镜像。
    
    Signed-off-by: Hao Chen <chenhaoa@uniontech.com>

--- scripts/live.sh
+++ scripts/live.sh
@@ -15,33 +15,28 @@ fi
 export MAKEFLAGS=-j$JOBS
 
 #1.创建“制作基地”
-export LIVEUSB=/haoos/liveusb
-rm -rf ${LIVEUSB}
-mkdir -pv $LIVEUSB
+export RELEASE=/haoos/release
+rm -rf ${RELEASE}
+mkdir -pv $RELEASE
 #2.创建制作相关的目录
 #（1）创建制作初始化辅助系统的目录
-mkdir -pv ${LIVEUSB}/tmpfs/{initramfs,initrd,tmp}
+mkdir -pv ${RELEASE}/initramfs
 #（2）创建制作最终文件系统的目录
-mkdir -pv ${LIVEUSB}/system
-#（3）创建制作ISO文件的目录，usb目录用于存放LiveUSB的内容，包括最终系统景象文件
+mkdir -pv ${RELEASE}/squashfs
+#（3）创建制作ISO文件的目录，live目录用于存放Live的内容，包括最终系统景象文件
 #boot目录存放GRUB相关的文件
 #live目录存放内核及辅助系统文件等
-mkdir -pv ${LIVEUSB}/usb/{boot,live}
-#（4）创建软件包编译目录
-mkdir -pv ${LIVEUSB}/{build,download}
+mkdir -pv ${RELEASE}/ISO/{boot,live}
 
-export BUILD_DIR=${LIVEUSB}/build
-export DOWNLOAD_DIR=${LIVEUSB}/download
-
-#liveusb filesystem build
-pushd ${LIVEUSB}/system
-echo "copying the files to system..."
+#RELEASE filesquashfs build
+pushd ${RELEASE}/squashfs
+echo "copying the files to squashfs..."
 #cp -a /{bin,dev,etc,home,lib,lib64,run,sbin,tmp,usr,var} ./
 #cp -a /{bin,dev,etc,lib,lib64,run,sbin,tmp,usr,var,opt} ./
 cp -a /{dev,etc,run,usr,var,opt} ./
 
-#如果主系统存在/lib64目录，则把/lib64内容移动到/system/usr/lib64下，并创建
-#/system/lib64软链接指向/system/usr/lib64.
+#如果主系统存在/lib64目录，则把/lib64内容移动到/squashfs/usr/lib64下，并创建
+#/squashfs/lib64软链接指向/squashfs/usr/lib64.
 ROOT_DIR="bin lib lib32 lib64 libx32 sbin"
 for root_dir in $ROOT_DIR
 do
@@ -54,16 +49,16 @@ done
 mkdir -vp media mnt proc root srv sys home tmp
 #创建默认用户haoos和root，密码为1
 set +e
-chroot ${LIVEUSB}/system rm -rf haoos
-chroot ${LIVEUSB}/system userdel haoos
-chroot ${LIVEUSB}/system groupdel haoos
-chroot ${LIVEUSB}/system groupadd haoos
-chroot ${LIVEUSB}/system useradd -m -s /bin/bash  -g haoos haoos
-echo "root:1" | chroot ${LIVEUSB}/system/ chpasswd
-echo "haoos:1" | chroot ${LIVEUSB}/system/ chpasswd
+chroot ${RELEASE}/squashfs rm -rf haoos
+chroot ${RELEASE}/squashfs userdel haoos
+chroot ${RELEASE}/squashfs groupdel haoos
+chroot ${RELEASE}/squashfs groupadd haoos
+chroot ${RELEASE}/squashfs useradd -m -s /bin/bash  -g haoos haoos
+echo "root:1" | chroot ${RELEASE}/squashfs/ chpasswd
+echo "haoos:1" | chroot ${RELEASE}/squashfs/ chpasswd
 set -e
 
-cat > ${LIVEUSB}/system/etc/haoos_defconf << "EOF"
+cat > ${RELEASE}/squashfs/etc/haoos_defconf << "EOF"
 #!/bin/bash
 gsettings set org.gnome.Terminal.Legacy.Settings theme-variant 'dark'
 
@@ -79,11 +74,11 @@ gsettings set org.gnome.desktop.input-sources sources "[('xkb', 'us'), ('ibus', 
 
 EOF
 
-chmod 777 ${LIVEUSB}/system/etc/haoos_defconf
+chmod 777 ${RELEASE}/squashfs/etc/haoos_defconf
 
-cat > ${LIVEUSB}/system/etc/systemd/system/hello.service << "EOF"
+cat > ${RELEASE}/squashfs/etc/systemd/system/hello.service << "EOF"
 # Simple service unit file to use for testing
-# startup configurations with systemd.
+# startup configurations with squashfsd.
 # By David Both
 # Licensed under GPL V2
 #
@@ -97,7 +92,7 @@ StandardOutput=journal+console
 WantedBy=multi-user.target
 EOF
 
-chroot ${LIVEUSB}/system systemctl enable hello.service
+chroot ${RELEASE}/squashfs systemctl enable hello.service
 
 rm -rf usr/src
 rm -rf lib/modules
@@ -105,55 +100,55 @@ rm -rf lib/modules
 rm -rf lib/udev/write_*
 rm -f etc/udev/rules.d/70*
 
-rm ${LIVEUSB}/system/etc/fstab
-cat > ${LIVEUSB}/system/etc/fstab << EOF
+rm -f ${RELEASE}/squashfs/etc/fstab
+cat > ${RELEASE}/squashfs/etc/fstab << EOF
 none	/proc		proc	defaults	0	0
 sysfs	/sys		sysfs	defaults	0	0
 devpts	/dev/pts	devpts	gid=4,mode=620	0	0
 #tmpfs	/dev/shm	tmpfs	defaults	0	0
 EOF
 
-rm ${LIVEUSB}/system/etc/mtab
-cat > ${LIVEUSB}/system/etc/mtab << EOF
+rm -f ${RELEASE}/squashfs/etc/mtab
+cat > ${RELEASE}/squashfs/etc/mtab << EOF
 none	/proc		proc	defaults	0	0
 sysfs	/sys		sysfs	defaults	0	0
 devpts	/dev/pts	devpts	gid=4,mode=620	0	0
 EOF
 
-#build liveusb kernel
+#build RELEASE kernel
 pushd /lfs/linux-5.10.17
 #拷贝内核文件到live目录
-cp -av arch/x86/boot/bzImage ${LIVEUSB}/usb/live/liveusb-kernel
+cp -av arch/x86/boot/bzImage ${RELEASE}/ISO/live/live-kernel
 #安装内核到文件系统
-make INSTALL_MOD_PATH=${LIVEUSB}/system/ INSTALL_MOD_STRIP=1 modules_install
+make INSTALL_MOD_PATH=${RELEASE}/squashfs/ INSTALL_MOD_STRIP=1 modules_install
 
 popd #linux-5.10.17
 #done
 
-popd #${LIVEUSB}/system
+popd #${RELEASE}/squashfs
 
-#build liveusb initramfs
+#build RELEASE initramfs
 CURRENT_DIR=`dirname $0`
-. $CURRENT_DIR/initramfs-liveusb.sh
+. $CURRENT_DIR/initramfs-live.sh
 #done
 
-#创建USB启动盘识别标签
-echo "HaoOS's LiveUSB" > ${LIVEUSB}/usb/LABEL
+#创建live启动盘识别标签
+echo "HaoOS's RELEASE" > ${RELEASE}/ISO/LABEL
 
 #将所需的文件和目录制作成镜像文件
-pushd ${LIVEUSB}/system
-mksquashfs * ${LIVEUSB}/usb/SYSTEM.img
-popd #${LIVEUSB}/system
+pushd ${RELEASE}/squashfs
+mksquashfs * ${RELEASE}/ISO/squashfs.img
+popd #${RELEASE}/squashfs
 
 #grub2 install
-pushd ${LIVEUSB}/usb
+pushd ${RELEASE}/ISO
 #1
 #mkdir -p boot/grub
-#2安装grub2的模块文件,这里从lfs系统拷贝，system没有字体文件。
+#2安装grub2的模块文件,这里从lfs系统拷贝，squashfs没有字体文件。
 cp -av /boot/grub boot/
 
 #3创建GRUB-2的启动配置文件
-cat > ${LIVEUSB}/usb/boot/grub/grub.cfg << "EOF"
+cat > ${RELEASE}/ISO/boot/grub/grub.cfg << "EOF"
 set default=0
 set timeout=5
 insmod vbe
@@ -179,12 +174,12 @@ loadfont ($root)/boot/grub/themes/Cyberpunk/terminus-18.pf2
 insmod png
 set theme=($root)/boot/grub/themes/Cyberpunk/theme.txt
 export theme
-menuentry "HaoOS's LiveUSB" {
+menuentry "HaoOS's RELEASE" {
 	set gfxpayload=keep
 	echo    '载入 Linux 5.10.17 ...'
-	linux	/live/liveusb-kernel
+	linux	/live/live-kernel
 	echo    '载入初始化内存盘...'
-	initrd	/live/liveusb-initramfs.img
+	initrd	/live/live-initramfs.img
 }
 menuentry '重启'{
 	reboot
@@ -197,19 +192,19 @@ menuentry '进入UEFI界面'{
 }
 EOF
 
-#4创建LiveUSB启动文件
+#4创建RELEASE启动文件
 #-p --prefix-directory.Set the $prefix variable
 export PREFIX=/boot/grub
-grub-mkimage -o ${LIVEUSB}/core.img -p $PREFIX -O i386-pc iso9660 linux biosdisk
-cat /usr/lib/grub/i386-pc/cdboot.img ${LIVEUSB}/core.img \
-	> ${LIVEUSB}/usb/boot/liveusb.img
-popd #${LIVEUSB}/usb
+grub-mkimage -o ${RELEASE}/core.img -p $PREFIX -O i386-pc iso9660 linux biosdisk
+cat /usr/lib/grub/i386-pc/cdboot.img ${RELEASE}/core.img \
+	> ${RELEASE}/ISO/boot/live.img
+popd #${RELEASE}/ISO
 
 #添加UEFI启动支持
-tar xf /sources/other-sources/EFI.tar.xz -C ${LIVEUSB}/usb/
+tar xf /sources/other-sources/EFI.tar.xz -C ${RELEASE}/ISO/
 
-#generate usb file
-pushd ${LIVEUSB}
-mkisofs -R -boot-info-table -no-emul-boot -boot-load-size 4 -b boot/liveusb.img -V "myliveusb" \
-	-o haoos-liveusb.iso usb
+#generate iso file
+pushd ${RELEASE}
+mkisofs -R -boot-info-table -no-emul-boot -boot-load-size 4 -b boot/live.img -V "mylive" \
+	-o haoos-live.iso ISO
 popd
