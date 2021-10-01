ee10c02cfa929e2f055b10c53f5507632ee2bdc0
Hao Chen <chenhaoa@uniontech.com>
2021-10-01 19:10:17 +0800

    os-build: install.sh: fix一些包名会导致安装失败
    如binutils-pass1修复成binutils.源码包都一样，只是.cmd安装脚本不一样.
    grep binutils-pass1获取压缩包名会失败.
    
    Signed-off-by: Hao Chen <chenhaoa@uniontech.com>

--- os-build/install.sh
+++ os-build/install.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-if [ `id -u` != 0 ];then                                                                                                                                                  
-        echo Permission delay, Please run as root!                                                                                                                        
-        exit                                                                                                                                                              
-fi
-
 if [ -n $JOBS ];then
 	JOBS=`grep -c ^processor /proc/cpuinfo 2>/dev/null`
 	if [ ! $JOBS ];then
@@ -12,23 +7,28 @@ if [ -n $JOBS ];then
 fi
 export MAKEFLAGS=-j$JOBS
 
+PACKAGES_PATH=build/pkg/packages.pkg
+if [ -z $PACKAGES_PATH ];then echo "Error: No packages.pkg file!" ;exit; fi
+
 install_pkg()
 {
 #@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@build
-url=$(grep $1 build/pkg/packages.pkg)
-#echo $url
+#获取本地软件压缩包路径
+url=$(grep $1 $PACKAGES_PATH)
+echo $url
 tar_pkg=$(echo $url | awk -F '/' '{print $NF}')
 #echo $tar_pkg
 #pkg_dir=$(echo $tar_pkg | awk -F '.tar.gz' '{print $1}')
 pkg_dir=$(echo $tar_pkg | awk -F '.tar' '{print $1}')
 
-#解压软件包
 pushd ./build
-#清除软件包目录
+#清除软件包目录,防止上次构建的残留造成影响。可以做成一个选项
 rm -rf $pkg_dir
-tar xf $tar_pkg
+#解压软件包
+tar -xf $tar_pkg
 pushd $pkg_dir
-sudo bash -e ../cmd/$pkg.cmd
+#执行软件包安装命令
+bash -e ../cmd/$pkg.cmd
 popd
 #清除软件包目录
 rm -rf $pkg_dir
@@ -41,12 +41,16 @@ case $1 in
 		if [ -z $2 ]
 		then
 			echo "Usage: -f <file>"	
+			echo "		从文件获取软件包安装列表"
 			exit
 		fi
 
 		for pkg in `cat $2`
 		do
-			install_pkg $pkg;
+			#fix包名，如binutils-pass1修复成binutils.源码包都一样，只是.cmd安装脚本不一样
+			pkg_name=$(basename -s -pass1 $pkg)
+			pkg_name=$(basename -s -pass2 $pkg_name)
+			install_pkg $pkg_name;
 		done
 		;;
 esac
