5de49bb969f82550976e6a0b4bbd174646c77d82
Hao Chen <chenhaoa@unionrwxh.com>
2021-10-05 19:27:49 +0800

    upgrade os-build scripts
    Signed-off-by: Hao Chen <chenhaoa@unionrwxh.com>

--- os-build/install.sh
+++ os-build/install.sh
@@ -14,8 +14,10 @@ install_pkg()
 {
 #@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@build
 #获取本地软件压缩包路径
-url=$(grep -A 3 -i "^$1" $PACKAGES_PATH | grep Download)
+#对于python这样的有doc压缩包，匹配不精确会有两个下载链接
+url=$(grep -A 3 -i "^$1 (" $PACKAGES_PATH | grep Download | head -1)
 if [ -z "$url" ];then echo Error: No "$1" url found in $PACKAGES_PATH!; exit; fi
+
 tar_pkg=$(echo $url | awk -F '/' '{print $NF}')
 #echo $tar_pkg
 #pkg_dir=$(echo $tar_pkg | awk -F '.tar.gz' '{print $1}')
@@ -26,6 +28,12 @@ pushd ./build/cmd
 rm -rf $pkg_dir
 #解压软件包
 tar -xf $tar_pkg
+#解压缩后的包名hook
+case "$pkg_dir" in
+tcl8.6.11-src)
+	pkg_dir="tcl8.6.11"
+	;;
+esac
 pushd $pkg_dir
 #执行软件包安装命令
 bash -e ../$CHAPTER/$pkg.cmd
@@ -55,21 +63,25 @@ case $1 in
 			#忽略-list文件中'#'开头的行
 			echo $pkg | grep "^#"
 			if [ $? -eq 0 ];then continue; fi
-			#pkg_name：对应源码压缩包;pkg：对应.cmd文件名
-			case $pkg in
+			#fix包名，如binutils-pass1修复成binutils.源码包都一样，只是.cmd安装脚本不一样
+			pkg_name=$(basename -s -pass1 $pkg)
+			pkg_name=$(basename -s -pass2 $pkg_name)
+			#pkg_name：对应url源码压缩包;pkg：对应.cmd文件名
+			case $pkg_name in
 			linux-headers)
 				pkg_name=linux
 				;;
 			libstdc++)
 				pkg_name=gcc
 				;;
+			xz)
+				pkg_name="Xz Utils"
+				;;
 			*)
-			#fix包名，如binutils-pass1修复成binutils.源码包都一样，只是.cmd安装脚本不一样
-				pkg_name=$(basename -s -pass1 $pkg)
-				pkg_name=$(basename -s -pass2 $pkg_name)
 				;;
 			esac
-			install_pkg $pkg_name;
+			#没有“”,xz utils入参会被判定为两个,$1=xz,$2=utils,报错
+			install_pkg "$pkg_name";
 		done
 		;;
 esac
--- scripts/chroot_do-7_6.sh
+++ scripts/chroot_do-7_6.sh
@@ -13,92 +13,15 @@ if [ -n $JOBS ];then
         fi
 fi
 export MAKEFLAGS=-j$JOBS
-pushd /lfs
 
 #7.7. GCC-10.2.0 中的 Libstdc++，第二遍
-pushd gcc-10.2.0
-ln -svf gthr-posix.h libgcc/gthr-default.h
-rm build -rf
-mkdir -v build
-pushd build
-../libstdc++-v3/configure            \
-    CXXFLAGS="-g -O2 -D_GNU_SOURCE"  \
-    --prefix=/usr                    \
-    --disable-multilib               \
-    --disable-nls                    \
-    --host=$(uname -m)-lfs-linux-gnu \
-    --disable-libstdcxx-pch
-make
-make install
-popd #build
-popd #gcc-10.2.0
-
 #7.8. Gettext-0.21
-pushd gettext-0.21
-./configure --disable-shared
-make
-cp -v gettext-tools/src/{msgfmt,msgmerge,xgettext} /usr/bin
-popd
-
 #7.9. Bison-3.7.5
-pushd bison-3.7.5
-./configure --prefix=/usr \
-            --docdir=/usr/share/doc/bison-3.7.5
-make
-make install
-popd
-
 #7.10. Perl-5.32.1
-pushd perl-5.32.1
-sh Configure -des                                        \
-             -Dprefix=/usr                               \
-             -Dvendorprefix=/usr                         \
-             -Dprivlib=/usr/lib/perl5/5.32/core_perl     \
-             -Darchlib=/usr/lib/perl5/5.32/core_perl     \
-             -Dsitelib=/usr/lib/perl5/5.32/site_perl     \
-             -Dsitearch=/usr/lib/perl5/5.32/site_perl    \
-             -Dvendorlib=/usr/lib/perl5/5.32/vendor_perl \
-             -Dvendorarch=/usr/lib/perl5/5.32/vendor_perl
-make
-make install
-popd
-
 #7.11. Python-3.9.2
-pushd Python-3.9.2
-./configure --prefix=/usr   \
-            --enable-shared \
-            --without-ensurepip
-make 
-make install
-popd
-
 #7.12. Texinfo-6.7
-pushd texinfo-6.7
-./configure --prefix=/usr
-make 
-make install
-popd
-
 #7.13. Util-linux-2.36.2
-pushd util-linux-2.36.2
-mkdir -pv /var/lib/hwclock
-./configure ADJTIME_PATH=/var/lib/hwclock/adjtime    \
-            --docdir=/usr/share/doc/util-linux-2.36.2 \
-            --disable-chfn-chsh  \
-            --disable-login      \
-            --disable-nologin    \
-            --disable-su         \
-            --disable-setpriv    \
-            --disable-runuser    \
-            --disable-pylibmount \
-            --disable-static     \
-            --without-python     \
-            runstatedir=/run
-make 
-make install
-popd
-
-popd #/lfs
+./os-build/install.sh -f ./os-build/lfs-list-chapter07
 
 #7.14. Cleaning up and Saving the Temporary System
 #find /usr/{lib,libexec} -name \*.la -delete
